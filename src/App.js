47b1155699039b11612ccd102dc3f415dc596420
Matej Džijan <matej.dzijan@gmail.com>
2020-03-19 02:13:32 +0100

    Check rotation, input, show solutions, stop at 100 solutions

--- src/App.js
+++ src/App.js
@@ -18,24 +18,24 @@ function useMergeState(initialState) {
   return [state, setMergedState]
 }
 
-const size = 4;
-const sqSize = Math.pow(size, 2)
-const ps = [pieces.ROOK, pieces.ROOK, pieces.KNIGHT, pieces.KNIGHT];
-
 function App() {
   const [state, setState] = useMergeState({
-    stack: [new Array(sqSize).fill(0)],
+    stack: null,
     indexes: [0],
-    lastIndexes: new Array(ps.length).fill(0),
+    lastIndexes: null,
     currentPieceIndex: 0,
     text: '',
     finished: false,
     solutions: [],
     superDone: false,
-    looping: false
+    looping: false,
+    showSolution: -1,
+    size: 0,
+    ps: []
   })
 
-  let { stack, lastIndexes, indexes, currentPieceIndex, text, finished, solutions, superDone, looping } = state
+  let { stack, lastIndexes, indexes, currentPieceIndex, text, finished, solutions, superDone, looping, showSolution, size, ps } = state
+  const sqSize = Math.pow(size, 2)
 
   const handleClick = () => {
     do {
@@ -59,7 +59,10 @@ function App() {
             finished,
             solutions,
             superDone,
-            looping
+            looping,
+            showSolution,
+            size,
+            ps
           })
         }
         continue
@@ -76,7 +79,7 @@ function App() {
       }
       if (index >= sqSize) {
         if (currentPieceIndex <= 0) {
-          text = 'Done!'
+          text = `Done! Found ${solutions.length} solutions.`
           superDone = true
           if (!looping) {
             setState({
@@ -88,7 +91,10 @@ function App() {
               finished,
               solutions,
               superDone,
-              looping
+              looping,
+              showSolution,
+              size,
+              ps
             })
           }
           continue
@@ -108,7 +114,10 @@ function App() {
             finished,
             solutions,
             superDone,
-            looping
+            looping,
+            showSolution,
+            size,
+            ps
           })
         }
       }
@@ -121,11 +130,18 @@ function App() {
         currentPieceIndex++
         text = ''
         if (currentPieceIndex >= ps.length) {
-          text = 'Found one, continuing...'
-          finished = true
           if (!containsArray(currentBoard, solutions)) {
+            text = 'Found one, continuing...'
+            finished = true
             solutions.push(currentBoard)
-            break
+            if (solutions.length >= 100) {
+              text = `Found 'first' 100 solutions!`
+              superDone = true
+              break;
+            }
+          }
+          else {
+            text = 'Duplicate solution, continuing...'
           }
         }
         if (!looping) {
@@ -138,7 +154,10 @@ function App() {
             finished,
             solutions,
             superDone,
-            looping
+            looping,
+            showSolution,
+            size,
+            ps
           })
         }
       }
@@ -153,7 +172,10 @@ function App() {
         finished,
         solutions,
         superDone,
-        looping
+        looping,
+        showSolution,
+        size,
+        ps
       })
     }
   }
@@ -169,7 +191,10 @@ function App() {
       finished,
       solutions,
       superDone,
-      looping
+      looping,
+      showSolution,
+      size,
+      ps
     })
     handleClick()
     looping = !looping
@@ -182,37 +207,121 @@ function App() {
       finished,
       solutions,
       superDone,
-      looping
+      looping,
+      showSolution,
+      size,
+      ps
     })
   }
 
   const reset = () => {
     setState({
-      stack: [new Array(sqSize).fill(0)],
+      stack: null,
       indexes: [0],
-      lastIndexes: new Array(ps.length).fill(0),
+      lastIndexes: null,
       currentPieceIndex: 0,
       text: '',
       finished: false,
       solutions: [],
       superDone: false,
-      looping: false
+      looping: false,
+      showSolution: -1,
+      size: 0,
+      ps: []
+    })
+  }
+
+  const handleStart = (input) => {
+    size = input.size
+    for (let i = 0; i < input.queens; i++) {
+      ps.push(pieces.QUEEN)
+    }
+    for (let i = 0; i < input.rooks; i++) {
+      ps.push(pieces.ROOK)
+    }
+    for (let i = 0; i < input.bishops; i++) {
+      ps.push(pieces.BISHOP)
+    }
+    for (let i = 0; i < input.knights; i++) {
+      ps.push(pieces.KNIGHT)
+    }
+    for (let i = 0; i < input.kings; i++) {
+      ps.push(pieces.KING)
+    }
+    if (ps.length === 0) {
+      ps.push(pieces.QUEEN)
+    }
+    stack = [new Array(Math.pow(size, 2)).fill(0)]
+    lastIndexes = new Array(ps.length).fill(0)
+    setState({
+      stack,
+      indexes,
+      lastIndexes,
+      currentPieceIndex,
+      text,
+      finished,
+      solutions,
+      superDone,
+      looping,
+      showSolution,
+      size,
+      ps
+    })
+  }
+
+  const cycleSolutions = (left) => {
+    if (left) {
+      showSolution = showSolution <= 0 ? 0 : showSolution - 1
+    }
+    else {
+      showSolution = showSolution >= solutions.length - 1 ? solutions.length - 1 : showSolution + 1
+    }
+    setState({
+      stack,
+      indexes,
+      lastIndexes,
+      currentPieceIndex,
+      text,
+      finished,
+      solutions,
+      superDone,
+      looping,
+      showSolution,
+      size,
+      ps
     })
   }
 
   return (
     <div className="App">
-      <Board field={stack[stack.length - 1]} width={20} />
-      <button onClick={handleClick} disabled={state.superDone}>Next step</button>
-      <button onClick={handleFindAll} disabled={state.superDone}>Find next solution</button>
+
+      <Board field={size === 0 ? [] : superDone && showSolution >= 0 ? solutions[showSolution] : stack[stack.length - 1]} width={20} handleSubmit={handleStart} size={size} pieces={countPieces(ps)} />
+      <button disabled={!(superDone && showSolution > 0)} onClick={() => { cycleSolutions(true) }}>{`<`}</button>
+      <button disabled={!(superDone && showSolution < (solutions.length - 1))} onClick={() => { cycleSolutions(false) }}>{`>`}</button>
+      <br />
+      <button onClick={handleClick} disabled={superDone || size === 0}>Next step</button>
+      <button onClick={handleFindAll} disabled={superDone || size === 0}>Find all solutions (max 100)</button>
       <p>{text}</p>
-      <button onClick={() => { console.log(state.solutions) }} disabled={!state.superDone}>Show solutions</button>
-      <button onClick={reset} >Reset</button>
+      <button onClick={reset} disabled={size === 0}>Reset</button>
     </div>
   );
 }
 
 export default App;
+
+function countPieces(array) {
+  const result = {
+    [`${pieces.QUEEN}`]: 0,
+    [`${pieces.ROOK}`]: 0,
+    [`${pieces.BISHOP}`]: 0,
+    [`${pieces.KNIGHT}`]: 0,
+    [`${pieces.KING}`]: 0,
+  }
+  for (let i = 0; i < array.length; i++) {
+    result[array[i]]++
+  }
+  return result
+}
 
 function check(piece, array, location) {
   switch (piece) {
@@ -226,6 +335,7 @@ function check(piece, array, location) {
       return checkKnight(array, location)
     case pieces.KING:
       return checkKing(array, location)
+    default:
   }
 }
 
@@ -294,6 +404,7 @@ function checkIfOk(piece, array, location) {
       return checkIfKnightOk(array, location)
     case pieces.KING:
       return checkIfKingOk(array, location)
+    default:
   }
 }
 
@@ -362,22 +473,38 @@ function checkIfQueenOk(array, location) {
 }
 
 function containsArray(array, arrayOfArrays) {
-  const size = Math.pow(array, 0.5)
+  const size = Math.pow(array.length, 0.5)
   let verticalMirror = []
-  let horizontalMirror = []
+  let r90 = [], r180 = [], r270 = []
+  let vmR90 = [], vmR180 = [], vmR270 = []
   for (let i = 0; i < array.length; i++) {
     const row = Math.floor(i / size)
     verticalMirror.push(array[size * (2 * row + 1) - 1 - i])
-    horizontalMirror.push(array[Math.floor(i - (2 * (row - (size - 1) / 2)) * size)])
+  }
+  for (let i = 0; i < array.length; i++) {
+    const row = Math.floor(i / size)
+    const column = i % size
+    const index = (size - 1) * size - column * size + row
+    r90.push(array[index])
+    vmR90.push(verticalMirror[index])
+  }
+  for (let i = 0; i < array.length; i++) {
+    const row = Math.floor(i / size)
+    const column = i % size
+    const index = (size - 1) * size - column * size + row
+    r180.push(r90[index])
+    vmR180.push(vmR90[index])
+  }
+  for (let i = 0; i < array.length; i++) {
+    const row = Math.floor(i / size)
+    const column = i % size
+    const index = (size - 1) * size - column * size + row
+    r270.push(r180[index])
+    vmR270.push(vmR180[index])
   }
   for (let a of arrayOfArrays) {
-    if (arraysEqual(a, array)) {
-      return true
-    }
-    if (arraysEqual(a, verticalMirror)) {
-      return true
-    }
-    if (arraysEqual(a, horizontalMirror)) {
+    if (arraysEqual(a, array) || arraysEqual(a, verticalMirror) || arraysEqual(a, r90) || arraysEqual(a, vmR90) ||
+      arraysEqual(a, r180) || arraysEqual(a, vmR180) || arraysEqual(a, r270) || arraysEqual(a, vmR270)) {
       return true
     }
   }
--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,14 @@
 import * as React from 'react'
 import { createUseStyles } from 'react-jss'
 
+const pieces = {
+    QUEEN: 'Q',
+    ROOK: 'R',
+    BISHOP: 'B',
+    KNIGHT: 'N',
+    KING: 'K'
+}
+
 const styles = {
     blackSquare: {
         width: size => size,
@@ -21,21 +29,132 @@ const styles = {
         borderColor: 'blue',
         padding: 0,
         color: 'red'
+    },
+    form: {
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        width: 8,
+        height: 3.6,
+        fontSize: 5,
+    },
+    label: {
+        fontSize: 8,
+        display: 'flex',
+        alignItems: 'center'
+    },
+    button: {
+        height: 10,
+        fontSize: 5,
+        padding: 0
     }
 }
 
 const useStyles = createUseStyles(styles)
 
 function Board(props) {
+    const [size, setSize] = React.useState(3)
+    const [queens, setQueens] = React.useState(0)
+    const [rooks, setRooks] = React.useState(0)
+    const [bishops, setBishops] = React.useState(0)
+    const [knights, setKnights] = React.useState(0)
+    const [kings, setKings] = React.useState(0)
     const classes = useStyles(props.width)
     const field = props.field
-    const size = Math.pow(field.length, 0.5)
+
+    React.useEffect(() => {
+        setQueens(props.pieces[pieces.QUEEN])
+        setRooks(props.pieces[pieces.ROOK])
+        setBishops(props.pieces[pieces.BISHOP])
+        setKnights(props.pieces[pieces.KNIGHT])
+        setKings(props.pieces[pieces.KING])
+        setSize(props.size < 3 ? 3 : props.size)
+    }, [props])
+
+    const handleChange = (event, piece) => {
+        let value = event.target.value
+        if (isNaN(value)) {
+            return
+        }
+        if (value === '') {
+            value = 0
+        }
+        switch (piece) {
+            case pieces.QUEEN:
+                setQueens(value)
+                break
+            case pieces.ROOK:
+                setRooks(value)
+                break
+            case pieces.BISHOP:
+                setBishops(value)
+                break
+            case pieces.KNIGHT:
+                setKnights(value)
+                break
+            case pieces.KING:
+                setKings(value)
+                break
+            default:
+        }
+    }
+
+    const handleSizeChange = (event) => {
+        let value = event.target.value
+        if (isNaN(value)) {
+            return
+        }
+        if (value < 3) {
+            value = 3
+        }
+        else if (value > 7) {
+            value = 7
+        }
+        setSize(value)
+    }
+
     return (
         <div>
+            <form onSubmit={(event) => {
+                event.preventDefault()
+                props.handleSubmit({
+                    size, queens, rooks, bishops, knights, kings
+                })
+            }} className={classes.form}>
+                <label className={classes.label}>
+                    Size:
+                    <input type="text" value={size} onChange={handleSizeChange} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <label className={classes.label}>
+                    Q:
+                    <input type="text" value={queens} onChange={(event) => { handleChange(event, pieces.QUEEN) }} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <label className={classes.label}>
+                    R:
+                    <input type="text" value={rooks} onChange={(event) => { handleChange(event, pieces.ROOK) }} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <label className={classes.label}>
+                    B:
+                    <input type="text" value={bishops} onChange={(event) => { handleChange(event, pieces.BISHOP) }} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <label className={classes.label}>
+                    N:
+                    <input type="text" value={knights} onChange={(event) => { handleChange(event, pieces.KNIGHT) }} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <label className={classes.label}>
+                    K:
+                    <input type="text" value={kings} onChange={(event) => { handleChange(event, pieces.KING) }} className={classes.input} disabled={props.size !== 0} />
+                </label>
+                <input type="submit" value="Start" className={classes.button} disabled={props.size !== 0} />
+            </form>
             {field.map((val, i) => {
                 return (
                     <React.Fragment key={`f${i}`}>
-                        {<button className={(i % 2) ? classes.whiteSquare : classes.blackSquare} disabled key={i}>
+
+                        {<button className={(i % size % 2) === (Math.floor(i / size) % 2) ? classes.whiteSquare : classes.blackSquare} disabled key={i}>
                             {val}
                         </button>}
                         {((i + 1) % size === 0) ? <br key={`br${i}`} /> : null}
